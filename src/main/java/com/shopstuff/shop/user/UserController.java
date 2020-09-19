a17f6682208f83dfb9a175e79cf50166a9f90f50
lavalumpa <zrnozrno1@gmail.com>
2020-09-19 18:30:00 +0200

    Validated User

--- src/main/java/com/shopstuff/shop/user/UserController.java
+++ src/main/java/com/shopstuff/shop/user/UserController.java
@@ -1,13 +1,11 @@
 package com.shopstuff.shop.user;
 
 
+import com.shopstuff.shop.exceptions.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
@@ -15,7 +13,6 @@ import java.net.URI;
 @RestController
 @RequestMapping("user")
 @RequiredArgsConstructor
-@Validated
 public class UserController {
 
     private final UserService userService;
