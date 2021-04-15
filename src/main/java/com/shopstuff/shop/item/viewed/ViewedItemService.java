021d757c11dc0f57c33b82ddaa51838265657711
lavalumpa <tomislav.tadic.89@gmail.com>
2021-04-15 03:43:15 +0200

    Documented ItemController

--- src/main/java/com/shopstuff/shop/item/viewed/ViewedItemService.java
+++ src/main/java/com/shopstuff/shop/item/viewed/ViewedItemService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -19,6 +20,7 @@ public class ViewedItemService {
     private final UserService userService;
     private final ViewedItemRepository viewedItemRepository;
 
+    @Transactional
     public void itemViewed(String name, Item item) {
         var user = userService.findByName(name).orElseThrow(NotFoundException::new);
         if (!user.getRoles().contains(Role.CUSTOMER)) {
@@ -35,6 +37,7 @@ public class ViewedItemService {
         );
     }
 
+    @Transactional
     public Page<Item> recentItemsByUser(int id, Pageable pageable) {
         var user = userService.findById(id).orElseThrow(NotFoundException::new);
         var page = viewedItemRepository.findByUser(user, pageable);
--- src/test/java/com/shopstuff/shop/item/ItemControllerTest.java
+++ src/test/java/com/shopstuff/shop/item/ItemControllerTest.java
@@ -12,13 +12,19 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -36,7 +42,6 @@ public class ItemControllerTest {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
 
-
     @Test
     @WithMockUser(username = "Steve")
     public void testGetItemWhenItemIsFoundWithGivenId() throws Exception {
@@ -49,19 +54,37 @@ public class ItemControllerTest {
                 .andExpect(jsonPath("$.id").value(item.getId()))
                 .andExpect(jsonPath("$.name").value(item.getName()))
                 .andExpect(jsonPath("$.price").value(item.getPrice()))
-                .andDo(document("documentation"));
+                .andDo(document("item/get-one-item",
+                        pathParameters(
+                                parameterWithName("id").description("The id of an item")),
+                        responseFields(
+                                fieldWithPath("id").description("Id of the item"),
+                                fieldWithPath("name").description("Name of the item"),
+                                fieldWithPath("price").description("Price of the item")
+                        )));
     }
+
+    private final ResponseFieldsSnippet responseFields = relaxedResponseFields(
+            subsectionWithPath("content").description("Contains the items retrieved"),
+            fieldWithPath("content[].id").description("Id of one item"),
+            fieldWithPath("content[].name").description("Name of the item"),
+            fieldWithPath("content[].price").description("Price of the item")
+    );
 
     @Test
     @WithMockUser
     public void TestGetAllItemsWhenThereIsOneItem() throws Exception {
         Item item = Item.builder().name("Phone").price(7000).build();
         item = itemRepository.save(item);
-        mockMvc.perform(get("/item"))
+        mockMvc.perform(get("/item?page={page}&size={size}", 0, 1))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content[0].id").value(item.getId()))
                 .andExpect(jsonPath("$.content[0].name").value(item.getName()))
-                .andExpect(jsonPath("$.content[0].price").value(item.getPrice()));
+                .andExpect(jsonPath("$.content[0].price").value(item.getPrice()))
+                .andDo(document("item/get-page", requestParameters(
+                        parameterWithName("page").description("Page of the items retrieved, default is 0"),
+                        parameterWithName("size").description("The amount of items per page, default is 20")
+                ), this.responseFields));
     }
 
     @Test
@@ -74,7 +97,20 @@ public class ItemControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(header().exists("Location"))
                 .andExpect(jsonPath("$.name").value(item.getName()))
-                .andExpect(jsonPath("$.price").value(item.getPrice()));
+                .andExpect(jsonPath("$.price").value(item.getPrice()))
+                .andDo(document("item/post-item",
+                        requestFields(
+                                fieldWithPath("id").description("Id of the item, not necessary as it will be provided"),
+                                fieldWithPath("name").description("Name of the item to be added"),
+                                fieldWithPath("price").description("Price of the item to be added")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("Id of the newly created item"),
+                                fieldWithPath("name").description("Name of the item that's going to be added"),
+                                fieldWithPath("price").description("Price of the item that's going to be added")
+                        ), responseHeaders(
+                                headerWithName("Location").description("The location of the newly created item")
+                        )));
     }
 
     @Test
@@ -82,11 +118,20 @@ public class ItemControllerTest {
     public void testSearchItemWithGivenName() throws Exception {
         Item item = Item.builder().name("Phone").price(7000).build();
         item = itemRepository.save(item);
-        mockMvc.perform(get("/item/search").param("q", "pho"))
+        mockMvc.perform(get("/item/search")
+                .param("q", "pho")
+                .param("page", "0")
+                .param("size", "0"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content[0].id").value(item.getId()))
                 .andExpect(jsonPath("$.content[0].name").value(item.getName()))
-                .andExpect(jsonPath("$.content[0].price").value(item.getPrice()));
+                .andExpect(jsonPath("$.content[0].price").value(item.getPrice()))
+                .andDo(document("item/search",
+                        requestParameters(
+                                parameterWithName("q").description("Name or partial name of the item's name to be searched"),
+                                parameterWithName("page").description("Page of the result, default 0"),
+                                parameterWithName("size").description("The amount of items per page, default is 10")
+                        ), this.responseFields));
     }
 
 
@@ -97,7 +142,11 @@ public class ItemControllerTest {
         item = itemRepository.save(item);
         mockMvc.perform(delete("/item/{id}", item.getId()).with(csrf()))
                 .andExpect(status().isNoContent())
-                .andExpect(jsonPath("$").doesNotExist());
+                .andExpect(jsonPath("$").doesNotExist())
+                .andDo(document("item/delete-item",
+                        pathParameters(
+                                parameterWithName("id").description("Id of the item to be deleted")
+                        )));
     }
 
 
@@ -112,7 +161,21 @@ public class ItemControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(item.getId()))
                 .andExpect(jsonPath("$.price").value(updatedItem.getPrice()))
-                .andExpect(jsonPath("$.name").value(updatedItem.getName()));
+                .andExpect(jsonPath("$.name").value(updatedItem.getName()))
+                .andDo(document("item/update",
+                        pathParameters(
+                                parameterWithName("id").description("Id of the item to be updated")
+                        ),
+                        requestFields(
+                                fieldWithPath("id").description("Id of the item that is going to be updated"),
+                                fieldWithPath("name").description("New name of the item"),
+                                fieldWithPath("price").description("New price of the item")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("Id of the changed item"),
+                                fieldWithPath("name").description("New name of the changed item"),
+                                fieldWithPath("price").description("New price of the item")
+                        )));
     }
 
 
