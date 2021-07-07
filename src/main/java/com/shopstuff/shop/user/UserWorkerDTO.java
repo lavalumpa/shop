4455584f3808432a9e8d08295a9c56a4ba7ddc8f
lavalumpa <tomislav.tadic.89@gmail.com>
2021-07-07 17:43:45 +0200

    Added UserWorkerDTO

---
+++ src/main/java/com/shopstuff/shop/user/UserWorkerDTO.java
@@ -0,0 +1,27 @@
+package com.shopstuff.shop.user;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+@Data
+public class UserWorkerDTO {
+
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    private Integer id;
+    @Size(min = 1, max = 50, message = "Name must be 1 to 50 characters long")
+    private String name;
+    @Email(message = "Email should be valid")
+    private String email;
+    @Size(min = 8, message = "Password must have at least 8 characters")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @Size(min=1, message = "Has to include at least 1 Role")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Set<Role> roles;
+}
