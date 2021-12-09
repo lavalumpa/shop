12debdf5fba895d64ff73085b12074fa641428c8
lavalumpa <tomislav.tadic.89@gmail.com>
2021-12-09 17:32:09 +0100

    Migrate to java 17

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM openjdk:16-jdk-alpine
+FROM openjdk:17-jdk-alpine
 COPY build/libs/shop-0.0.1-SNAPSHOT.jar shop-0.0.1-SNAPSHOT.jar
 ENTRYPOINT ["java","-jar","/shop-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/shopstuff/shop/ShopApplication.java
+++ src/main/java/com/shopstuff/shop/ShopApplication.java
@@ -3,9 +3,7 @@ package com.shopstuff.shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableJpaAuditing
